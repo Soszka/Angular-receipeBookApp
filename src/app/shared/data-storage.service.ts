c946c1f6bbb9c2356e96966a0362b762e83fa09f
Soszka <bsocha1998@gmail.com>
2023-11-28 10:29:17 +0100

    sending the signup request created

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -13,7 +13,7 @@ export class DataStorageService {
     const recipes = this.recipeService.getRecipes();
     this.http
       .put(
-        'https://newproject-d9ca6-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json',
         recipes
       )
       .subscribe(response => {
@@ -24,7 +24,7 @@ export class DataStorageService {
   fetchRecipes() {
     return this.http
       .get<Recipe[]>(
-        'https://newproject-d9ca6-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
       )
       .pipe(
         map(recipes => {
